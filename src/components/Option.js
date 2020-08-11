d80feb07450164ab1251595c47503760caad6fe7
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-08-11 23:49:12 +0530

    loc error

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,18 +1,24 @@
 import React from "react";
 
 class Option extends React.Component {
+  state = { lat: "", lon: "", error: "" };
   currentLocation = () => {
     navigator.geolocation.getCurrentPosition(
       (pos) => {
-        console.log(pos);
+        console.log(pos.coords.latitude);
+        console.log(pos.coords.longitude);
       },
       (err) => {
-        console.log(err);
+        console.log(err.message);
+        this.setState({ error: err.message });
       }
     );
   };
 
   render() {
+    if (this.state.error) {
+      return <div>{this.state.error}</div>;
+    }
     return (
       <div>
         <button onClick={this.currentLocation}>Detect</button>
